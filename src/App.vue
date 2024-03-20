bd5ab81afe1f406839e6144c83b5b1e3834c3112
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-20 13:18:35 +0100

    [add] scaffolding & Hello world

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data(){
+    return {
+      title: 'Hello World!!!'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>Welcome to Vite</p>
+  </div>
+</template>
+
+<style>
+
+</style>
